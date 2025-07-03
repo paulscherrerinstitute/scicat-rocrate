0ced1b24a68e9c4b6a421c5bb1387e63871105be
fpotier <frederic.potier@protonmail.com>
2025-07-03 11:00:14 +0200

    Change zenodo export media type to json-ld

--- src/main/java/ch/psi/scicat/zenodo/ZenodoController.java
+++ src/main/java/ch/psi/scicat/zenodo/ZenodoController.java
@@ -1,11 +1,11 @@
 package ch.psi.scicat.zenodo;
 
+import ch.psi.scicat.ExtraMediaType;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("zenodo")
@@ -19,7 +19,7 @@ public class ZenodoController {
 
     @GET
     @Path("/{doi}/export")
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(ExtraMediaType.APPLICATION_JSONLD)
     public Response exportPublication(@PathParam("doi") String doi) {
         return Response.ok(exporter.exportPublication(doi).toString()).build();
     }
