78a40c50e1a476b7ec04c4b49a7dad377e605ece
fpotier <frederic.potier@protonmail.com>
2025-12-01 16:06:39 +0100

    fix login in ITs

--- src/test/java/ch/psi/ord/api/EndpointTest.java
+++ src/test/java/ch/psi/ord/api/EndpointTest.java
@@ -4,6 +4,7 @@ import ch.psi.scicat.client.ScicatClient;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.test.InjectMock;
+import jakarta.inject.Inject;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URI;
@@ -12,9 +13,18 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 public abstract class EndpointTest {
   @InjectMock protected ScicatClient scicatClient;
+
+  @Inject
+  @ConfigProperty(name = "scicat.v4")
+  boolean backendV4;
+
+  private HttpClient client = HttpClient.newHttpClient();
+  private final String loginPayload = "{\"username\":\"rocrate\", \"password\":\"rocrate\"}";
+  private ObjectMapper objectMapper = new ObjectMapper();
 
   protected String accessToken = "";
 
@@ -22,20 +32,17 @@ public abstract class EndpointTest {
 
   public String login() {
     try {
-      String url = "http://backend.localhost/api/v3/Users/login";
-      String jsonInputString = "{\"username\":\"rocrate\", \"password\":\"rocrate\"}";
-      HttpClient client = HttpClient.newHttpClient();
+      String url = "http://backend.localhost/api/v3/" + (backendV4 ? "auth" : "Users") + "/login";
       HttpRequest request =
           HttpRequest.newBuilder()
               .uri(URI.create(url))
               .header("Content-Type", "application/json")
               .header("Accept", "application/json")
-              .POST(HttpRequest.BodyPublishers.ofString(jsonInputString))
+              .POST(HttpRequest.BodyPublishers.ofString(loginPayload))
               .build();
       HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-      ObjectMapper objectMapper = new ObjectMapper();
       JsonNode jsonResponse = objectMapper.readTree(response.body());
-      return jsonResponse.get("id").asText();
+      return jsonResponse.get(backendV4 ? "access_token" : "id").asText();
     } catch (IOException | InterruptedException e) {
       throw new RuntimeException("Failed to fetch SciCat access token, aborting tests");
     }
