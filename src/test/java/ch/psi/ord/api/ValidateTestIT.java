f6cde03f81c229de01d836b6454d9b15b98e6eee
fpotier <58594145+fpotier@users.noreply.github.com>
2026-05-07 17:15:16 +0200

    test: manage scicatlive as a test resource (#216)
    test: manage scicatlive as a test resource

--- src/test/java/ch/psi/ord/api/ValidateTestIT.java
+++ src/test/java/ch/psi/ord/api/ValidateTestIT.java
@@ -1,8 +1,12 @@
 package ch.psi.ord.api;
 
+import ch.psi.scicat.ScicatLive;
+import io.quarkus.test.common.TestResourceScope;
+import io.quarkus.test.common.WithTestResource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
+@WithTestResource(value = ScicatLive.class, scope = TestResourceScope.MATCHING_RESOURCES)
 public class ValidateTestIT extends ValidateTest {
   public ValidateTestIT() {
     accessToken = login();
