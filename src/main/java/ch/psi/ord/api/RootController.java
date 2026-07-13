100b0201ab57ebbebbd36e58e47af3ad4ab3341c
fpotier <frederic.potier@protonmail.com>
2026-07-13 09:50:01 +0200

    get the path from the config

--- src/main/java/ch/psi/ord/api/RootController.java
+++ src/main/java/ch/psi/ord/api/RootController.java
@@ -4,11 +4,15 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
 import java.net.URI;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/")
 public class RootController {
+  @ConfigProperty(name = "quarkus.swagger-ui.path", defaultValue = "swagger-ui")
+  String swaggerUiPath;
+
   @GET
   public Response redirectToSwaggerUi() {
-    return Response.seeOther(URI.create("/explorer")).build();
+    return Response.seeOther(URI.create(swaggerUiPath)).build();
   }
 }
