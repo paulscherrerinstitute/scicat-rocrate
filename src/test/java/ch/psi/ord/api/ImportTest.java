751f46ae6c5017e25928b570b694f12bbbee6f84
fpotier <frederic.potier@protonmail.com>
2026-07-20 14:43:38 +0200

    refactor: import tests

--- src/test/java/ch/psi/ord/api/ImportTest.java
+++ src/test/java/ch/psi/ord/api/ImportTest.java
@@ -1,6 +1,7 @@
 package ch.psi.ord.api;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -13,135 +14,200 @@ import ch.psi.scicat.model.v3.CreatePublishedDataDto;
 import ch.psi.scicat.model.v3.Dataset;
 import ch.psi.scicat.model.v3.PublishedData;
 import io.quarkus.test.junit.QuarkusTest;
-import java.io.IOException;
-import org.hamcrest.Matchers;
+import io.restassured.response.ValidatableResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import lombok.Data;
 import org.jboss.resteasy.reactive.RestResponse;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.FieldSource;
 
 @QuarkusTest
 public class ImportTest extends EndpointTest {
-  @Test
-  @DisplayName("No Accept header")
-  public void test00() {
-    given().when().post("/api/v1/ro-crate/import").then().statusCode(415);
+  @Data
+  static class ImportTestCase {
+    String testName;
+    String contentType;
+    boolean includeApiKey;
+    byte[] body;
+    int expectedStatusCode;
+    Consumer<ImportTest> mockSetup;
+    Consumer<ValidatableResponse> assertions;
+
+    @Override
+    public String toString() {
+      return String.format("[%s] - %s", testName, contentType);
+    }
   }
 
-  @Test
-  @DisplayName("Empty body")
-  public void test01() {
-    if (scicatClient != null) {
-      when(scicatClient.checkTokenValidity(any())).thenReturn(true);
+  private static final Consumer<ImportTest> NO_MOCK_SETUP = test -> {};
+
+  private static final Consumer<ValidatableResponse> NO_ASSERTIONS = res -> {};
+
+  private static final String PUBLICATION_DOI = "10.16907/d910159a-d48a-45fb-acf2-74b27cd5a8e5";
+
+  private static Consumer<ImportTest> checkTokenValidity(boolean isValid) {
+    return test -> {
+      if (test.scicatClient != null) {
+        when(test.scicatClient.checkTokenValidity(any())).thenReturn(isValid);
+      }
+    };
+  }
+
+  private static final Consumer<ImportTest> MOCK_NEW_PUBLICATION =
+      checkTokenValidity(true)
+          .andThen(
+              test -> {
+                if (test.scicatClient != null) {
+                  when(test.scicatClient.countPublishedData(
+                          String.format(
+                              RoCrateImporter.publicationExistsFilter,
+                              DoiUtils.buildStandardUrl(PUBLICATION_DOI)),
+                          null))
+                      .thenReturn(RestResponse.ok(new CountResponse().setCount(0)));
+                  when(test.scicatClient.myidentity(any()))
+                      .thenReturn(RestResponse.ok(TestData.rocrateUser));
+                  when(test.scicatClient.createDataset(any(), any(CreateDatasetDto.class)))
+                      .thenReturn(RestResponse.ok(new Dataset().setPid("some-pid")));
+                  when(test.scicatClient.createPublishedData(
+                          any(), any(CreatePublishedDataDto.class)))
+                      .thenReturn(RestResponse.ok(new PublishedData().setDoi("some-pid")));
+                  when(test.scicatClient.registerPublishedData(any(), any()))
+                      .thenReturn(RestResponse.ok());
+                }
+              });
+
+  private static final Consumer<ImportTest> MOCK_EXISTING_PUBLICATION =
+      checkTokenValidity(true)
+          .andThen(
+              test -> {
+                if (test.scicatClient != null) {
+                  when(test.scicatClient.countPublishedData(
+                          String.format(
+                              RoCrateImporter.publicationExistsFilter,
+                              DoiUtils.buildStandardUrl(PUBLICATION_DOI)),
+                          null))
+                      .thenReturn(RestResponse.ok(new CountResponse().setCount(1)));
+                }
+              });
+
+  private static ImportTestCase createTestCase(
+      String testName,
+      String contentType,
+      boolean includeApiKey,
+      byte[] body,
+      int expectedStatusCode,
+      Consumer<ImportTest> mockSetup,
+      Consumer<ValidatableResponse> assertions) {
+    return new ImportTestCase()
+        .setTestName(testName)
+        .setContentType(contentType)
+        .setIncludeApiKey(includeApiKey)
+        .setBody(body)
+        .setExpectedStatusCode(expectedStatusCode)
+        .setMockSetup(mockSetup)
+        .setAssertions(assertions);
+  }
+
+  static List<ImportTestCase> testMatrix =
+      new ArrayList<>() {
+        {
+          add(
+              createTestCase(
+                  "No Accept header", null, false, null, 415, NO_MOCK_SETUP, NO_ASSERTIONS));
+
+          add(
+              createTestCase(
+                  "Empty body",
+                  ExtraMediaType.APPLICATION_JSONLD,
+                  true,
+                  null,
+                  400,
+                  checkTokenValidity(true),
+                  NO_ASSERTIONS));
+
+          add(
+              createTestCase(
+                  "Invalid JSON-LD",
+                  ExtraMediaType.APPLICATION_JSONLD,
+                  true,
+                  "{".getBytes(),
+                  400,
+                  checkTokenValidity(true),
+                  NO_ASSERTIONS));
+
+          add(
+              createTestCase(
+                  "Empty JSON-LD",
+                  ExtraMediaType.APPLICATION_JSONLD,
+                  true,
+                  "{}".getBytes(),
+                  400,
+                  checkTokenValidity(true),
+                  NO_ASSERTIONS));
+
+          add(
+              createTestCase(
+                  "Unauthenticated",
+                  ExtraMediaType.APPLICATION_JSONLD,
+                  false,
+                  getResource("one-publication.json"),
+                  401,
+                  checkTokenValidity(false),
+                  NO_ASSERTIONS));
+
+          add(
+              createTestCase(
+                  "One publication",
+                  ExtraMediaType.APPLICATION_JSONLD,
+                  true,
+                  getResource("one-publication.json"),
+                  201,
+                  MOCK_NEW_PUBLICATION,
+                  res -> res.body("$", hasKey(PUBLICATION_DOI))));
+
+          add(
+              createTestCase(
+                  "Import existing publication",
+                  ExtraMediaType.APPLICATION_JSONLD,
+                  true,
+                  getResource("one-publication.json"),
+                  409,
+                  MOCK_EXISTING_PUBLICATION,
+                  NO_ASSERTIONS));
+        }
+      };
+
+  @ParameterizedTest
+  @FieldSource("testMatrix")
+  @DisplayName("Import Matrix Execution")
+  public void testImportMatrix(ImportTestCase testCase) {
+    testCase.getMockSetup().accept(this);
+    ValidatableResponse response = executeImportRequest(testCase);
+    testCase.getAssertions().accept(response);
+  }
+
+  private ValidatableResponse executeImportRequest(ImportTestCase testCase) {
+    var request = given();
+
+    if (testCase.getContentType() != null) {
+      request = request.header("Content-Type", testCase.getContentType());
     }
 
-    given()
-        .header("Content-Type", ExtraMediaType.APPLICATION_JSONLD)
-        .header("api-key", accessToken)
+    if (testCase.isIncludeApiKey()) {
+      request = request.header("api-key", accessToken);
+    }
+
+    if (testCase.getBody() != null) {
+      request = request.body(testCase.getBody());
+    }
+
+    return request
         .when()
         .post("/api/v1/ro-crate/import")
         .then()
-        .statusCode(400);
-  }
-
-  @Test
-  @DisplayName("Invalid JSON-LD")
-  public void test02() {
-    if (scicatClient != null) {
-      when(scicatClient.checkTokenValidity(any())).thenReturn(true);
-    }
-
-    given()
-        .header("Content-Type", ExtraMediaType.APPLICATION_JSONLD)
-        .header("api-key", accessToken)
-        .body("{")
-        .when()
-        .post("/api/v1/ro-crate/import")
-        .then()
-        .statusCode(400);
-  }
-
-  @Test
-  @DisplayName("Empty JSON-LD")
-  public void test03() {
-    if (scicatClient != null) {
-      when(scicatClient.checkTokenValidity(any())).thenReturn(true);
-    }
-
-    given()
-        .header("Content-Type", ExtraMediaType.APPLICATION_JSONLD)
-        .header("api-key", accessToken)
-        .body("{}")
-        .when()
-        .post("/api/v1/ro-crate/import")
-        .then()
-        .statusCode(400);
-  }
-
-  @Test
-  @DisplayName("Unauthenticated")
-  public void test04() throws IOException, Exception {
-    if (scicatClient != null) {
-      when(scicatClient.checkTokenValidity(any())).thenReturn(false);
-    }
-
-    given()
-        .header("Content-Type", ExtraMediaType.APPLICATION_JSONLD)
-        .body(getClass().getClassLoader().getResourceAsStream("one-publication.json"))
-        .when()
-        .post("/api/v1/ro-crate/import")
-        .then()
-        .statusCode(401);
-  }
-
-  @Test
-  @DisplayName("One publication")
-  public void test05() {
-    if (scicatClient != null) {
-      when(scicatClient.checkTokenValidity(any())).thenReturn(true);
-      when(scicatClient.countPublishedData(
-              String.format(
-                  RoCrateImporter.publicationExistsFilter,
-                  DoiUtils.buildStandardUrl("10.16907/d910159a-d48a-45fb-acf2-74b27cd5a8e5")),
-              null))
-          .thenReturn(RestResponse.ok(new CountResponse().setCount(0)));
-      when(scicatClient.myidentity(any())).thenReturn(RestResponse.ok(TestData.rocrateUser));
-      when(scicatClient.createDataset(any(), any(CreateDatasetDto.class)))
-          .thenReturn(RestResponse.ok(new Dataset().setPid("some-pid")));
-      when(scicatClient.createPublishedData(any(), any(CreatePublishedDataDto.class)))
-          .thenReturn(RestResponse.ok(new PublishedData().setDoi("some-pid")));
-      when(scicatClient.registerPublishedData(any(), any())).thenReturn(RestResponse.ok());
-    }
-
-    given()
-        .header("Content-Type", ExtraMediaType.APPLICATION_JSONLD)
-        .header("api-key", accessToken)
-        .body(getClass().getClassLoader().getResourceAsStream("one-publication.json"))
-        .when()
-        .post("/api/v1/ro-crate/import")
-        .then()
-        .statusCode(201)
-        .body("$", Matchers.hasKey("10.16907/d910159a-d48a-45fb-acf2-74b27cd5a8e5"));
-  }
-
-  @Test
-  @DisplayName("Import existing publication")
-  public void test06() {
-    if (scicatClient != null) {
-      when(scicatClient.checkTokenValidity(any())).thenReturn(true);
-      when(scicatClient.countPublishedData(
-              String.format(
-                  RoCrateImporter.publicationExistsFilter,
-                  DoiUtils.buildStandardUrl("10.16907/d910159a-d48a-45fb-acf2-74b27cd5a8e5")),
-              null))
-          .thenReturn(RestResponse.ok(new CountResponse().setCount(1)));
-    }
-    given()
-        .header("Content-Type", ExtraMediaType.APPLICATION_JSONLD)
-        .header("api-key", accessToken)
-        .body(getClass().getClassLoader().getResourceAsStream("one-publication.json"))
-        .when()
-        .post("/api/v1/ro-crate/import")
-        .then()
-        .statusCode(409);
+        .statusCode(testCase.getExpectedStatusCode());
   }
 }
