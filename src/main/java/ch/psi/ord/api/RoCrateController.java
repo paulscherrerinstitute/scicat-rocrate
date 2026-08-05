5ff17041c807bb693df768f9ed340b0171d7b4ad
fpotier <58594145+fpotier@users.noreply.github.com>
2026-08-05 15:37:34 +0200

    refactor: remove unnecessary constructor (#307)

--- src/main/java/ch/psi/ord/api/RoCrateController.java
+++ src/main/java/ch/psi/ord/api/RoCrateController.java
@@ -49,11 +49,6 @@ public class RoCrateController {
 
   @Inject ScicatClient scicatClient;
 
-  public RoCrateController(RoCrateExporter exporter, RoCrateImporter importer) {
-    this.exporter = exporter;
-    this.importer = importer;
-  }
-
   @ServerExceptionMapper
   public Response mapRiotException(RiotException e) {
     log.error("Failed to process the crate metadata", e);
