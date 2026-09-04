18e00d016392b3baf700f899134ddf48a60b3a86
fpotier <58594145+fpotier@users.noreply.github.com>
2026-09-04 10:47:35 +0200

    fix(zenodo): don't crash when s3-broker returns `urls: null` (#348)

--- src/test/java/ch/psi/ord/api/ZenodoControllerTest.java
+++ src/test/java/ch/psi/ord/api/ZenodoControllerTest.java
@@ -9,11 +9,13 @@ import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import ch.psi.ord.core.DoiUtils;
 import ch.psi.scicat.TestData;
+import io.quarkus.test.junit.DisabledOnIntegrationTest;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.ws.rs.WebApplicationException;
 import org.apache.jena.vocabulary.SchemaDO;
@@ -124,6 +126,30 @@ public class ZenodoControllerTest extends EndpointTest {
         .body(
             SchemaDO.distribution.getLocalName() + "." + SchemaDO.expires.getLocalName(),
             isDateExpired());
+  }
+
+  @Test
+  @DisabledOnIntegrationTest()
+  @DisplayName("Export should succeed if the s3 broker reports no url for a dataset")
+  public void test03() {
+    when(scicatService.getPublishedDataById(TestData.psiPub1.getDoi()))
+        .thenReturn(RestResponse.ok(TestData.psiPub1));
+    when(s3BrokerService.getPublishedDataUrls(TestData.psiPub1.getDoi()))
+        .thenReturn(TestData.s3ResponseWithoutUrls);
+
+    String doiUrl = String.format(DoiUtils.buildStandardUrl(TestData.psiPub1.getDoi()));
+    given()
+        .when()
+        .accept(ExtraMediaType.APPLICATION_JSONLD)
+        .pathParam("doi", TestData.psiPub1.getDoi())
+        .get("/api/v1/zenodo/{doi}/export")
+        .then()
+        .statusCode(200)
+        .body("@type", equalTo(SchemaDO.Dataset.getLocalName()))
+        .body("@id", equalTo(doiUrl))
+        .body(SchemaDO.name.getLocalName(), equalTo(TestData.psiPub1.getTitle()))
+        .body(SchemaDO.distribution.getLocalName(), nullValue())
+        .body(SchemaDO.expires.getLocalName(), isDateExpired());
   }
 
   @Test
